7c44d92c96f8bafd8d0271800f88bb4468de589b
MaximilianLae <20230979@novaims.unl.pt>
2025-05-01 15:28:46 +0100

    improvement

--- src/report_generator.py
+++ src/report_generator.py
@@ -82,7 +82,7 @@ class SurfReportGenerator:
             "- Provides recommendations for surfers of various skill levels\n"
             "- Uses smooth transitions to connect the forecast with spot details\n\n"
             "Pay close attention to faithfulness, answer relevancy, and context relevancy.\n\n"
-            "Pay a lot of attention to the query that the user gives so you react really to his preferences."
+            "Pay a lot of attention to the query that the user gives so you react really to his preferences, this means the user should get the feeling that you react really personally to his query and preferences."
             f"User Query: {user_query}"
         )
 
